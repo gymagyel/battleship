3b54efbc9251abef391abfacad968e3b40285e98
gymagyel <227876535+gymagyel@users.noreply.github.com>
2026-05-18 13:11:40 +0100

    Prevent duplicate computer attacks

--- src/Player.js
+++ src/Player.js
@@ -4,9 +4,29 @@ export default class Player {
   constructor(isComputer = false) {
     this.gameboard = new Gameboard();
     this.isComputer = isComputer;
+      this.previousAttacks = [];
   }
 
   attack(enemy, x, y){
     enemy.gameboard.receiveAttack(x, y);
   }
+
+  randomAttack(enemy) {
+  let x;
+  let y;
+  let alreadyAttacked = true;
+
+  while (alreadyAttacked) {
+    x = Math.floor(Math.random() * 10);
+    y = Math.floor(Math.random() * 10);
+
+    alreadyAttacked = this.previousAttacks.some(
+      (attack) => attack.x === x && attack.y === y
+    );
+  }
+
+  this.previousAttacks.push({ x, y });
+
+  this.attack(enemy, x, y);
+}
 }
--- tests/player.test.js
+++ tests/player.test.js
@@ -27,3 +27,47 @@ test('player can attack enemy gameboard', () => {
   expect(ship.hits).toBe(1);
 });
 
+test('computer can make an attack', () => {
+  const computer = new Player(true);
+  const enemy = new Player();
+
+  computer.randomAttack(enemy);
+
+  const totalAttacks =
+    enemy.gameboard.missedAttacks.length;
+
+  expect(totalAttacks).toBe(1);
+});
+
+test('computer stores previous attacks', () => {
+  const computer = new Player(true);
+  const enemy = new Player();
+
+  computer.randomAttack(enemy);
+
+  expect(computer.previousAttacks.length).toBe(1);
+});
+
+test('computer does not attack same coordinate twice', () => {
+  const computer = new Player(true);
+  const enemy = new Player();
+
+  jest
+    .spyOn(Math, 'random')
+    .mockReturnValueOnce(0.1)
+    .mockReturnValueOnce(0.1)
+    .mockReturnValueOnce(0.1)
+    .mockReturnValueOnce(0.1)
+    .mockReturnValueOnce(0.5)
+    .mockReturnValueOnce(0.5);
+
+  computer.randomAttack(enemy);
+  computer.randomAttack(enemy);
+
+expect(computer.previousAttacks).toEqual([
+  { x: 1, y: 1 },
+  { x: 5, y: 5 },
+]);
+
+  Math.random.mockRestore();
+});
